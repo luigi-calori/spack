6abf480cff9778c98c936545b6dac101785ef02e
Luigi Calori <luigi.calori@gmail.com>
2015-12-23 13:08:05 +0100

    update libjpeg-turbo version

--- var/spack/packages/libjpeg-turbo/package.py
+++ var/spack/packages/libjpeg-turbo/package.py
@@ -6,15 +6,16 @@ class LibjpegTurbo(Package):
        decompression. libjpeg is a library that implements JPEG image
        encoding, decoding and transcoding."""
     homepage = "http://libjpeg-turbo.virtualgl.org"
-    url      = "http://downloads.sourceforge.net/libjpeg-turbo/libjpeg-turbo-1.3.1.tar.gz"
+    url      = "http://sourceforge.net/projects/libjpeg-turbo/files/1.4.2/libjpeg-turbo-1.4.2.tar.gz/download"
 
-    version('1.3.1', '2c3a68129dac443a72815ff5bb374b05')
+    version('1.4.2', '86b0d5f7507c2e6c21c00219162c3c44')
 
     # Can use either of these.
     depends_on("yasm")
     depends_on("nasm")
 
     def install(self, spec, prefix):
-        configure("--prefix=%s" % prefix)
+        configure('--prefix=%s' % prefix)
+
         make()
         make("install")
